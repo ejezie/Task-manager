c6e98531e82804de285cb63525fb9a87ddfe269b
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-01-23 20:01:05 +0100

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,33 @@ const Nav = styled.div`
   width: 100vw;
   height: 4rem;
   color: white;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 const Logo = styled.h1`
   margin-left: 3rem;
   letter-spacing: 2px;
-  margin-right: 0.4rem;
+  margin-right: 1.5rem;
+  font-size: 1.8rem;
+  @media (max-width: 960px){
+      font-size: 1rem;
+      margin-left: 1rem;
+  }
+`;
+const Heading = styled.h1`
+  font-size: 2rem;
+  @media (max-width: 960px) {
+    font-size: 1.2rem;
+  }
 `;
 
 function Navbar() {
   return (
     <Nav>
       <Logo>MiniTaskManager</Logo>
-      <Logo>
+      <Heading>
         <i class="fas fa-thumbtack"></i>
-      </Logo>
+      </Heading>
     </Nav>
   );
 }
