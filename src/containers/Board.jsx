252697c3d8bcd726427f5909d4cdffadfcf4dda6
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-01-23 21:23:35 +0100

    default lane design

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -5,11 +5,36 @@ width: 100vw;
 height: 90vh;
 border-left: 1.4rem solid rgba(255, 255, 255, 0.7);
 `;
-
+const LaneF = styled.div`
+  width: 20rem;
+  height: 90vh;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 0px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  position: relative;
+  &:after {
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 1px;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.5);
+    top: 5rem;
+  }
+`;
 
 function Board() {
   return <BoardWrap>
-
+      <LaneF/>
   </BoardWrap>;
 }
 
